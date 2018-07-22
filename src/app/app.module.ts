98d4eb666a816721e3dab92aab8487e24fdd3d23
Lucas Dobson <lucas@selltime.com>
2018-07-22 20:46:22 +1000

    First Commit - Created Angular app through the CLI

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { DragulaModule } from 'ng2-dragula';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    DragulaModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
